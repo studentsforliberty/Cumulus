17c06477def618d46b30eb9b0c53304633801a82
Beth Breisnes <bbreisnes@salesforce.com>
2018-08-22 16:33:31 -0700

    form on top, donor concatenated in grid

--- src/aura/batchPOC/batchPOCHelper.js
+++ src/aura/batchPOC/batchPOCHelper.js
@@ -6,7 +6,16 @@
         action.setCallback(this,function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                component.set("v.data", response.getReturnValue());
+                var rows = [];
+                response.getReturnValue().forEach(function(currentRow){
+                    if(currentRow.Donation_Donor__c === 'Account1') {
+                        currentRow.Donor = currentRow.Account1Imported__c;
+                    } else {
+                        currentRow.Donor = currentRow.Contact1Imported__c;
+                    }
+                    rows.push(currentRow);
+                });
+                component.set("v.data", rows);
             }
         });
         $A.enqueueAction(action);
