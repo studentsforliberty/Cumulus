aae69c7eb11255d7e251b8753e2633d6de548da4
Randi Wilson <randi.wilson@salesforce.com>
2018-08-24 09:54:27 -0700

    Refactored for namespace, created model for controller, and added flexipage default.

--- src/aura/batchPOC/batchPOCController.js
+++ src/aura/batchPOC/batchPOCController.js
@@ -1,36 +1,10 @@
 ({
     doInit: function (component, event, helper) {
         //creating datatable columns
-        component.set('v.columns', [
-            {label: 'Donor', fieldName: 'Donor', type: 'text', editable: false},
-            //{label: 'Contact', fieldName: 'Contact1Imported__c', type: 'text', editable: false},
-            //{label: 'Account', fieldName: 'Account1Imported__c', type: 'text', editable: false},
-            {label: 'Amount', fieldName: 'Donation_Amount__c', type: 'currency', editable: true},
-            {label: 'Donation Date', fieldName: 'Donation_Date__c', type: 'date', editable: true},
-            {label: 'Payment Method', fieldName: 'Payment_Method__c', type: 'text', editable: true},
-            {label: 'Check Number', fieldName: 'Payment_Check_Reference_Number__c', type: 'text', editable: true}
-            ,
-            {
-                label: 'Action', type: 'button', initialWidth: 135, typeAttributes:
-                    {label: 'Delete', name: 'view_details', title: 'Click to View or Edit Details'}
-            }
-        ]);
         //getting DataImport__c records from server by calling helper methods
-        helper.getDIs(component);
+        helper.getModel(component);
+        //helper.getDIs(component);
         helper.setDonorToContact(component);
-    },
-
-    updateTable: function (component, event, helper) {
-
-        var rows = component.get("v.data");
-        var myDI = component.get("v.rowId");
-        console.log(myDI);
-        for (var i = 0; i < rows.length; i++) {
-            console.log(rows[i]);
-            if (rows[i].id === myDI.Id) {
-                rows[i].FirstName = myDI.Contact1_Firstname__c;
-            }
-        }
     },
 
     onSubmit: function (component, event, helper) {
@@ -40,9 +14,7 @@
 
     onSuccess: function (component, event, helper) {
         helper.getDIs(component);
-        var toastEvent = $A.get("e.force:showToast");
-        toastEvent.setParams({"title": "Success!","message": "The property's info has been updated.","type": "success"});
-        toastEvent.fire();
+        helper.showToast(component, 'Success', "New donor information has been added to the data import batch.");
         component.set("v.hasActiveRow",false);
         component.set("v.hasActiveRow",true);
         helper.setDonorToContact(component);
@@ -58,5 +30,18 @@
         } else {
             helper.setDonorToAccount(component);
         }
-    }
+    },
+
+    updateTable: function (component, event, helper) {
+
+        var rows = component.get("v.data");
+        var myDI = component.get("v.rowId");
+        console.log(myDI);
+        for (var i = 0; i < rows.length; i++) {
+            console.log(rows[i]);
+            if (rows[i].id === myDI.Id) {
+                rows[i].FirstName = myDI.Contact1_Firstname__c;
+            }
+        }
+    },
 })
--- src/aura/batchPOC/batchPOCHelper.js
+++ src/aura/batchPOC/batchPOCHelper.js
@@ -1,28 +1,40 @@
 ({
-//this function will fetch Car__c records from server
+    clearRow: function (component, event) {
+        //component.set(.recordId, null);
+    },
+
     getDIs: function (component) {
         var action = component.get("c.getDataImports");
         action.setParams({batchId: component.get("v.recordId")});
         action.setCallback(this, function (response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                var rows = [];
-                response.getReturnValue().forEach(function (currentRow) {
-                    if (currentRow.Donation_Donor__c === 'Account1') {
-                        currentRow.Donor = currentRow.Account1Imported__c;
-                    } else {
-                        currentRow.Donor = currentRow.Contact1Imported__c;
-                    }
-                    rows.push(currentRow);
-                });
-                component.set("v.data", rows);
+                this.setDataTableRows(component, response.getReturnValue());
+            } else {
+                console.log('error: ' + response);
+                this.showToast(component, 'Error', response.getReturnValue());
             }
         });
         $A.enqueueAction(action);
     },
 
-    clearRow: function (component, event) {
-        //component.set(.recordId, null);
+    getModel: function(component) {
+        var action = component.get("c.getDataImportModel");
+        action.setParams({batchId: component.get("v.recordId")});
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var response = JSON.parse(response.getReturnValue());
+                component.set("v.labels", response.labels);
+                this.setDataTableRows(component, response.dataImportRows);
+                this.setColumns(component, response.columns);
+                this.setFormFields(component, response.columns);
+                component.set("v.hasActiveRow", true);
+            } else {
+                this.showToast(component, 'Error', response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
     },
 
     setDonorToContact: function (component) {
@@ -38,6 +50,44 @@
 
         var accountDiv = component.find("accountDiv");
         $A.util.removeClass(accountDiv, 'slds-hide');
+    },
+
+    setDataTableRows: function(component, responseRows) {
+        var rows = [];
+        responseRows.forEach(function (currentRow) {
+            if (currentRow.Donation_Donor__c === 'Account1') {
+                currentRow.Donor = currentRow.Account1Imported__c;
+            } else {
+                currentRow.Donor = currentRow.Contact1Imported__c;
+            }
+            rows.push(currentRow);
+        });
+        component.set("v.data", rows);
+    },
+
+    setColumns: function(component, dataColumns) {
+        var columns = [];
+        columns.push({label: 'Donor', fieldName: 'Donor', type: 'text', editable: false});
+
+        dataColumns.forEach(function(col){
+            columns.push({label: col.label, fieldName: col.fieldName, type: col.type, editable: col.editable});
+        });
+
+        columns.push({label: 'Action', type: 'button', initialWidth: 135, typeAttributes:
+            {label: 'Delete', name: 'view_details', title: 'Click to View or Edit Details'}
+        });
+
+        component.set('v.columns', columns);
+    },
+
+    setFormFields: function (component, dataColumns) {
+        var formFields = [];
+
+        dataColumns.forEach(function(field){
+            formFields.push({label: field.label, name: field.fieldName});
+        });
+
+        component.set('v.formFields', formFields);
     },
 
     saveDataImportRecord: function (component, event) {
@@ -59,7 +109,17 @@
 
         eventFields['NPSP_Data_Import_Batch__c'] = component.get("v.recordId");
 
-        component.find('recordViewForm').submit(eventFields);
+        component.find('recordEditForm').submit(eventFields);
 
+    },
+
+    showToast: function(component, type, message) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": type,
+            "message": message,
+            "type": type.toLowerCase()
+        });
+        toastEvent.fire();
     }
 })
